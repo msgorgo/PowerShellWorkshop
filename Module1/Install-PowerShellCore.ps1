b762eb9f7eb192292cb3a6f36133660140cffd19
Georg Drobny <georg.drobny@microsoft.com>
2022-05-04 14:21:36 +0200

    Update Install-PowerShellCore.ps1

--- Module1/Install-PowerShellCore.ps1
+++ Module1/Install-PowerShellCore.ps1
@@ -1,5 +1,5 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$uri = 'https://github.com/PowerShell/PowerShell/releases/download/v7.0.0/PowerShell-7.0.0-win-x64.msi'
+$uri = 'https://github.com/PowerShell/PowerShell/releases/download/v7.2.3/PowerShell-7.2.3-win-x64.msi'
 $outFile = $uri.Split("/")[-1]
 New-Item -Path C:\Temp -Type Directory
 Invoke-WebRequest -Uri $uri -OutFile "C:\Temp\$outFile" -UseBasicParsing -Verbose
